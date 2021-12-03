11fef7ae11d24124563da4ec2fa51c47bdfde66d
Young799 <guschoeu@student.gu.se>
2021-12-03 11:26:07 +0100

    mqtt set up

---
+++ app.js
@@ -0,0 +1,55 @@
+const mqtt = require("mqtt");
+const mongoose = require("mongoose");
+
+let mongoURI =
+    process.env.MONGODB_URI ||
+    "mongodb+srv://userone:123@cluster0.izgcz.mongodb.net/Dit355?retryWrites=true&w=majority";
+
+mongoose.connect(
+    mongoURI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err) {
+        if (err) {
+            console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
+            console.error(err.stack);
+            process.exit(1);
+        }
+        console.log(`Connected to MongoDB with URI: ${mongoURI}`);
+    }
+);
+
+//User
+//Check
+
+let User = require("./models/User");
+
+//MQTT
+let options = { clientId: "mqtt03", clean: true };
+
+let client = mqtt.connect("mqtt://localhost:1883", options);
+
+let topic = "newappointment";
+let topicResponse = "allnewappointment";
+
+let data = [];
+
+client.on("connect", () => {
+    console.log("Connected Now!!");
+    client.subscribe([topic], () => {
+        console.log(`Subscribed to ${topic}`);
+    });
+});
+
+client.on(
+    "message",
+    (topic, payload) => {
+        // data = payload.toJSON();
+        // for (item in data){
+        // console.log(payload)}
+
+        console.log(payload.toString());
+
+
+    }
+    // console.log("Message: ", topic, payload.toString());
+);
