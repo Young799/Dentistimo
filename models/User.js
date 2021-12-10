1f2f912c83bde10485db92fcca8ff01f0a674c8e
Young799 <guschoeu@student.gu.se>
2021-12-10 11:18:02 +0100

    new request added

--- models/User.js
+++ models/User.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String
+const UserSchema = new Schema({
+  Fname: { type: String },
+  Lname: {type: String},
+  id: {type: String},
+  email: {type: String},
+  password: {type: String}
 });
 
 module.exports = mongoose.model("User", UserSchema);
